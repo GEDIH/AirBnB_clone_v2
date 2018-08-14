aa6eb5b383e08c90e9a678123e0de9d2134f0a4e
bchen528 <bchen720@gmail.com>
2018-08-14 03:14:30 +0000

    first commit

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """tear down app context"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_states():
+    """lists states from database
+    Returns:
+        HTML
+    """
+    all_states = storage.all(State)
+    return render_template('8-cities_by_states.html', all_states=all_states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
