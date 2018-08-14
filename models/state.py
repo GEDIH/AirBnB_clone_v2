0ddc0d50cb7380118263ba1cf4a4f9fc308b7f10
bchen528 <bchen720@gmail.com>
2018-08-14 01:25:33 +0000

    modify cities getter

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
     Implementation of the State class
-'''
+"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
