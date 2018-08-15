3a04cd3840889315e3a7586293acc6bc0b7aa6e2
bchen528 <bchen720@gmail.com>
2018-08-15 05:54:12 +0000

    use list of states instead of dict for Jinja

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -21,14 +21,17 @@ def show_page():
     Returns:
         HTML
     """
-    all_states = storage.all(State)
-    all_cities = storage.all(City)
+    dict_states = storage.all(State)
     dict_amenities = storage.all(Amenity)
+    all_states = []
     all_amenities = []
+
+    for k, v in dict_states.items():
+        all_states.append(v)
     for k, v in dict_amenities.items():
         all_amenities.append(v)
     return render_template('10-hbnb_filters.html', all_states=all_states,
-                           all_cities=all_cities, all_amenities=all_amenities)
+                           all_amenities=all_amenities)
 
 
 if __name__ == "__main__":
