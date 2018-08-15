7e8a73fe02dd0211131fb77fd04ea24f9f79388b
bchen528 <bchen720@gmail.com>
2018-08-15 03:49:42 +0000

    change cities getter to return only cities for current state

--- models/state.py
+++ models/state.py
@@ -34,9 +34,7 @@ class State(BaseModel, Base):
             """
             list_cities = []
             all_cities = models.storage.all(City)
-            all_states = models.storage.all(State)
-            for k, state_obj in all_states.items():
-                for key, city_obj in all_cities.items():
-                    if city_obj.state_id == state_obj.id:
-                        list_cities.append(city_obj)
+            for key, city_obj in all_cities.items():
+                if city_obj.state_id == self.id:
+                    list_cities.append(city_obj)
             return list_cities
